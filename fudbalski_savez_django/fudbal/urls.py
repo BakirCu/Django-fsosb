0d0203759c1611eebb7437ce466db9ba12ebbd98
Bakir <bakir.feniks@gmail.com>
2019-09-15 16:57:31 +0200

    Napravio sam sve html stranice i poceo da ih uredjujem

--- fudbalski_savez_django/fudbal/urls.py
+++ fudbalski_savez_django/fudbal/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="pocetna_strana"),
-    path('gallery/', views.gallery, name="gallery"),
     path('savez/', views.savez, name="o_savezu"),
-
+    path('rukovodstvo/', views.rukovodstvo, name="rukovodstvo"),
+    path('propisi/', views.propisi, name='propisi'),
+    path('liga/', views.liga, name="liga"),
+    path('kup/', views.kup, name='kup'),
+    path('deligiranje/', views.deligiranje_sudija, name='deligiranje_sudija'),
+    path('lista/', views.lista_sudija, name='lista_sudija'),
+    path('vesti/', views.vesti, name='vesti'),
+    path('gallery/', views.gallery, name="gallery"),
 ]
--- fudbalski_savez_django/fudbal/views.py
+++ fudbalski_savez_django/fudbal/views.py
@@ -5,9 +5,37 @@ def home(request):
     return render(request, 'fudbal/home.html')
 
 
-def gallery(request):
-    return render(request, 'fudbal/gallery.html')
-
-
 def savez(request):
     return render(request, 'fudbal/o_savezu.html')
+
+
+def rukovodstvo(request):
+    return render(request, 'fudbal/rukovodstvo.html')
+
+
+def propisi(request):
+    return render(request, 'fudbal/propisi.html')
+
+
+def liga(request):
+    return render(request, 'fudbal/liga.html')
+
+
+def kup(request):
+    return render(request, 'fudbal/kup.html')
+
+
+def deligiranje_sudija(request):
+    return render(request, 'fudbal/deligiranje_sudija.html')
+
+
+def lista_sudija(request):
+    return render(request, 'fudbal/lista_sudija.html')
+
+
+def vesti(request):
+    return render(request, 'fudbal/vesti.html')
+
+
+def gallery(request):
+    return render(request, 'fudbal/gallery.html')
