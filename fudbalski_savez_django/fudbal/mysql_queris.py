06b5d8ca9a78fa10997cb1c4670caa8a90da3401
Bakir <bakir.feniks@gmail.com>
2019-09-27 21:15:46 +0200

    Napravio tabelu lige, ali nije sortirana

--- fudbalski_savez_django/fudbal/mysql_queris.py
+++ fudbalski_savez_django/fudbal/mysql_queris.py
@@ -3,7 +3,56 @@ from django.db import connection
 
 class Query:
 
-    def bodovi(ime_tima):
+    def odigrane_utakmice(ime_tima):
+        with connection.cursor() as cursor:
+            cursor.execute('''SELECT count(*)  FROM fudbal_sb.fudbal_utakmica
+                            INNER JOIN fudbal_tim
+                            ON fudbal_utakmica.domacin_id = fudbal_tim.id OR fudbal_utakmica.gost_id = fudbal_tim.id
+                            WHERE fudbal_tim.ime_tima= %s; ''', [ime_tima])
+
+            broj_odigrane_utacmice = cursor.fetchone()
+        return broj_odigrane_utacmice[0]
+
+    def pobede(ime_time):
+        with connection.cursor() as cursor:
+            cursor.execute('''SELECT COUNT(*) FROM fudbal_utakmica
+                            INNER JOIN fudbal_tim
+                            ON fudbal_utakmica.domacin_id = fudbal_tim.id OR fudbal_utakmica.gost_id = fudbal_tim.id
+                            WHERE fudbal_tim.ime_tima = %s
+                             AND ((fudbal_utakmica.domacin_id=fudbal_tim.id AND fudbal_utakmica.domacin_gol > fudbal_utakmica.gost_gol)
+                              OR
+                            (fudbal_utakmica.gost_id=fudbal_tim.id AND fudbal_utakmica.gost_gol > fudbal_utakmica.domacin_gol));''', [ime_time])
+
+            broj_pobeda = cursor.fetchone()
+        return broj_pobeda[0]
+
+    def porazi(ime_time):
+        with connection.cursor() as cursor:
+            cursor.execute('''SELECT COUNT(*) FROM fudbal_utakmica
+                            INNER JOIN fudbal_tim
+                            ON fudbal_utakmica.domacin_id = fudbal_tim.id OR fudbal_utakmica.gost_id = fudbal_tim.id
+                            WHERE fudbal_tim.ime_tima = %s
+                             AND ((fudbal_utakmica.domacin_id=fudbal_tim.id AND fudbal_utakmica.domacin_gol < fudbal_utakmica.gost_gol)
+                              OR
+                            (fudbal_utakmica.gost_id=fudbal_tim.id AND fudbal_utakmica.gost_gol < fudbal_utakmica.domacin_gol));''', [ime_time])
+
+            broj_porazi = cursor.fetchone()
+        return broj_porazi[0]
+
+    def nereseno(ime_time):
+        with connection.cursor() as cursor:
+            cursor.execute('''SELECT COUNT(*) FROM fudbal_utakmica
+                            INNER JOIN fudbal_tim
+                            ON fudbal_utakmica.domacin_id = fudbal_tim.id OR fudbal_utakmica.gost_id = fudbal_tim.id
+                            WHERE fudbal_tim.ime_tima = %s
+                             AND ((fudbal_utakmica.domacin_id=fudbal_tim.id AND fudbal_utakmica.domacin_gol = fudbal_utakmica.gost_gol)
+                              OR
+                            (fudbal_utakmica.gost_id=fudbal_tim.id AND fudbal_utakmica.gost_gol = fudbal_utakmica.domacin_gol));''', [ime_time])
+
+            broj_porazi = cursor.fetchone()
+        return broj_porazi[0]
+
+    def dati_golovi(ime_tima):
         with connection.cursor() as cursor:
             cursor.execute('''SELECT SUM(golovi) FROM
                             (SELECT SUM(fudbal_utakmica.domacin_gol) AS golovi FROM fudbal_sb.fudbal_utakmica
@@ -15,6 +64,30 @@ class Query:
                             ON fudbal_utakmica.gost_id=fudbal_tim.id
                              WHERE fudbal_tim.ime_tima = %s) AS bodovi''', [ime_tima, ime_tima])
 
-            broj_bodova = cursor.fetchone()[0]
+            broj_bodova = cursor.fetchone()
+            if not broj_bodova[0]:
+                return 0
+        return broj_bodova[0]
 
-        return broj_bodova
+    def primljeni_golovi(ime_tima):
+        with connection.cursor() as cursor:
+            cursor.execute('''SELECT SUM(golovi) FROM
+                            (SELECT SUM(fudbal_utakmica.gost_gol) AS golovi FROM fudbal_sb.fudbal_utakmica
+                            INNER JOIN fudbal_tim
+                            ON fudbal_utakmica.domacin_id=fudbal_tim.id
+                            WHERE fudbal_tim.ime_tima= %s UNION ALL
+                            SELECT SUM(fudbal_utakmica.domacin_gol ) FROM fudbal_sb.fudbal_utakmica
+                            INNER JOIN fudbal_tim
+                            ON fudbal_utakmica.gost_id=fudbal_tim.id
+                             WHERE fudbal_tim.ime_tima = %s) AS bodovi''', [ime_tima, ime_tima])
+
+            broj_bodova = cursor.fetchone()
+            if not broj_bodova[0]:
+                return 0
+        return broj_bodova[0]
+
+    def imena_timova():
+        with connection.cursor() as cursor:
+            cursor.execute('''SELECT fudbal_tim.ime_tima FROM fudbal_tim''')
+            imena_timova = cursor.fetchall()
+        return imena_timova
---
+++ fudbalski_savez_django/fudbal/tabela_lige.py
@@ -0,0 +1,14 @@
+
+
+class Liga():
+
+    def __init__(self, ime_tima, odigrane_utakmice, broj_pobeda, broj_neresenih, broj_poraza, dati_golovi, primljni_golovi, gol_razliak, bodovi):
+        self.ime_tima = ime_tima
+        self.odigrane_utakmice = odigrane_utakmice
+        self.broj_pobeda = broj_pobeda
+        self.broj_neresenih = broj_neresenih
+        self.broj_poraza = broj_poraza
+        self.dati_golovi = dati_golovi
+        self.primljni_golovi = primljni_golovi
+        self.gol_razliak = gol_razliak
+        self.bodovi = bodovi
