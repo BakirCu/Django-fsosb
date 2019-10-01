bc6d11a2cdc780d3064e4a1ec823a3b673ecbffc
Bakir <bakir.feniks@gmail.com>
2019-10-01 20:10:31 +0200

    dodao else

--- fudbalski_savez_django/fudbal/views.py
+++ fudbalski_savez_django/fudbal/views.py
@@ -20,15 +20,18 @@ def propisi(request):
 
 
 def liga(request):
+    # ovde uzimam sva kola i izdvajam broj kola i cuvam ih u listi 'brojevi_kola'
     sva_kola = Utakmica.objects.values('kolo').distinct()
     brojevi_kola = []
     for kolo in sva_kola:
         broj = kolo.get('kolo')
         brojevi_kola.append(broj)
-
+    # u zavisnosti od broja koji dobijem kada neko klikne na dugme, uzmem sve utakmice iz tog kola i prikazem ih
     if request.GET:
         broj_kola_str = request.GET.get('broj')
         utakmice_izabranog_kola = Utakmica.objects.all().filter(kolo=int(broj_kola_str))
+    else:
+        utakmice_izabranog_kola = Utakmica.objects.all().filter(kolo=int(1))
 
     tabela_utakmica = Liga.tabela_timova()
     tabela_utakmica.sort(key=lambda x: x.bodovi, reverse=True)
