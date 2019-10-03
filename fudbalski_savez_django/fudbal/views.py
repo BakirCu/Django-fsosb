06dafcaeb8dc60136ebf801bc6493beec4db1b13
Bakir <bakir.feniks@gmail.com>
2019-10-03 15:43:20 +0200

    naparvio tri for petelje za liga kolapse

--- fudbalski_savez_django/fudbal/views.py
+++ fudbalski_savez_django/fudbal/views.py
@@ -21,23 +21,23 @@ def propisi(request):
 
 def liga(request):
     # ovde uzimam sva kola i izdvajam broj kola i cuvam ih u listi 'brojevi_kola'
-    sva_kola = Utakmica.objects.values('kolo').distinct()
+    sva_kola = Utakmica.objects.filter(
+        sezona='2019/2020').values('kolo').distinct()
+    print(sva_kola)
     brojevi_kola = []
     for kolo in sva_kola:
-        broj = kolo.get('kolo')
-        brojevi_kola.append(broj)
-    # u zavisnosti od broja koji dobijem kada neko klikne na dugme, uzmem sve utakmice iz tog kola i prikazem ih
-    if request.GET:
-        broj_kola_str = request.GET.get('broj')
-        utakmice_izabranog_kola = Utakmica.objects.all().filter(kolo=int(broj_kola_str))
-    else:
-        utakmice_izabranog_kola = Utakmica.objects.all().filter(kolo=int(1))
+        utacmice_kola = {}
+        broj_key = kolo.get('kolo')
+        utakmice_izabranog_kola_value = Utakmica.objects.all().filter(kolo=int(broj_key))
+        utacmice_kola[broj_key] = utakmice_izabranog_kola_value
+        brojevi_kola.append(utacmice_kola)
 
+    print(brojevi_kola)
     tabela_utakmica = Liga.tabela_timova()
-    print(tabela_utakmica)
+
     return render(request, 'fudbal/liga.html', {'timovi': tabela_utakmica,
                                                 'broj_kola': brojevi_kola,
-                                                'kola': utakmice_izabranog_kola, })
+                                                })
 
 
 def kup(request):
