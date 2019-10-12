1aef6096d3d457c032631a5d717b561ba078bdc0
Bakir <bakir.feniks@gmail.com>
2019-10-13 00:18:23 +0200

    Mislim da sam uradio proveru stinga

--- fudbalski_savez_django/fudbal/my_functions.py
+++ fudbalski_savez_django/fudbal/my_functions.py
@@ -9,3 +9,47 @@ def dohvati_kola(last_season, objects):
             kolo=int(broj_kola), sezona=last_season)
         kola_list.append(utakmice_izabranog_kola)
     return kola_list
+
+
+def after_first_ch(some_txt, index):
+    lower_ch = []
+    while index < len(some_txt):
+        if some_txt[index].islower():
+            ch = some_txt[index]
+            lower_ch.append(ch)
+            index += 1
+        elif some_txt[index].isspace():
+            break
+        else:
+            print('posle velikog slova ide malo slovo')
+    while index < len(some_txt) and some_txt[index].isspace():
+        index += 1
+    lower_ch.append(' ')
+    return ''.join(lower_ch), index
+
+
+def is_valid(some_txt):
+    index = 0
+    final_list = []
+    while index < len(some_txt):
+        while index < len(some_txt) and some_txt[index].isspace():
+            index += 1
+        while index < len(some_txt) and some_txt[index].isalpha():
+            if some_txt[index].isupper():
+                final_list.append(some_txt[index])
+                index += 1
+                temp_str, index = after_first_ch(some_txt, index)
+                final_list.append(temp_str)
+            else:
+                print('Prvo slovo mora da bude veliko')
+
+    return ''.join(final_list)
+
+
+def main():
+    test_str = '  By   Br   Knn    '
+    valid = is_valid(test_str)
+    print(valid)
+
+
+main()
