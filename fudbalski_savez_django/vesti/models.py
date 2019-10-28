a1cbb3a06d1c40d518cdc1bb775f0333186f5272
Bakir <bakir.feniks@gmail.com>
2019-10-28 15:29:31 +0100

    Napravio da se za video cuva ceo embd kod

--- fudbalski_savez_django/vesti/models.py
+++ fudbalski_savez_django/vesti/models.py
@@ -7,14 +7,10 @@ class Vesti(models.Model):
     sadrzaj = models.TextField()
     vreme_posta = models.DateTimeField(default=timezone.now)
     slika = models.ImageField(default='default.jpg', upload_to='vesti_img')
-    video = models.CharField(max_length=100, blank=True, null=True)
+    video = models.CharField(max_length=300, blank=True, null=True)
 
     def __str__(self):
         return f"{self.naslov}"
-
-    def save(self, *args, **kwargs):
-        self.video = 'https://www.youtube.com/embed/' + str(self.video[-11:])
-        return super(Vesti, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Vesti'
