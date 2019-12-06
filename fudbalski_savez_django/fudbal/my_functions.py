0986d079b3bab689febcee09a4bc01ce08323fc4
Bakir <bakir.feniks@gmail.com>
2019-12-06 01:58:37 +0100

    Mozda jos poboljsao kod my_function

--- fudbalski_savez_django/fudbal/my_functions.py
+++ fudbalski_savez_django/fudbal/my_functions.py
@@ -17,7 +17,8 @@ class Provajder():
     def utakmice_kola(self, kolo, poslednja_sezona, tip_sezone):
         return Utakmica.objects.all().filter(kolo=int(kolo),
                                              sezona__sezona=poslednja_sezona,
-                                             sezona__tip_id__tip=tip_sezone).order_by('-datum_zakazano')
+                                             sezona__tip_id__tip=tip_sezone).order_by(
+                                                 '-datum_zakazano')
 
 
 class UtakmiceKola():
@@ -31,33 +32,31 @@ class UtakmiceKola():
                                                          tip_sezone)
         utakmice_kola = []
         for kolo in kola_poslednje_sezone:
-            aktivini_timovi_list = UtakmiceKola.aktive_tims(aktivini_timovi)
+            aktivini_timovi_set = UtakmiceKola.aktive_tims(aktivini_timovi)
             broj_kola = kolo.get('kolo')
             utakmice = self.provajder.utakmice_kola(broj_kola,
                                                     poslednja_sezona,
                                                     tip_sezone)
 
             slobodni_timovi = UtakmiceKola.remove_from_list(utakmice,
-                                                            aktivini_timovi_list)
+                                                            aktivini_timovi_set)
             utakmice_kola.append(UtakmiceSlobodniTimovi(utakmice,
                                                         slobodni_timovi))
         return utakmice_kola
 
     @staticmethod
-    def remove_from_list(utakmice, aktivini_timovi_list):
+    def remove_from_list(utakmice, aktivini_timovi):
         for utakmica in utakmice:
-            if utakmica.domacin.ime in aktivini_timovi_list:
-                aktivini_timovi_list.remove(utakmica.domacin.ime)
-            if utakmica.gost.ime in aktivini_timovi_list:
-                aktivini_timovi_list.remove(utakmica.gost.ime)
-        return aktivini_timovi_list
+            aktivini_timovi.discard(utakmica.domacin.ime)
+            aktivini_timovi.discard(utakmica.gost.ime)
+        return list(aktivini_timovi)
 
     @staticmethod
     def aktive_tims(timovi):
-        rezultat = []
+        rezultat = set()
         for tim in timovi:
             ime_tima = tim.get('tim_id__ime')
-            rezultat.append(ime_tima)
+            rezultat.add(ime_tima)
         return rezultat
 
 
