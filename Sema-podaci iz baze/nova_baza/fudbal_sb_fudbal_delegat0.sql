6cd7b2633259014ddfe9bc85ac25eca9117957a6
Bakir <bakir.feniks@gmail.com>
2019-12-13 15:02:08 +0100

    Nova baza

---
+++ Sema-podaci iz baze/nova_baza/fudbal_sb_fudbal_delegat0.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+--
+-- Host: localhost    Database: fudbal_sb
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `fudbal_delegat`
+--
+
+LOCK TABLES `fudbal_delegat` WRITE;
+/*!40000 ALTER TABLE `fudbal_delegat` DISABLE KEYS */;
+INSERT INTO `fudbal_delegat` VALUES (1,'Goran','Marinković','1969-10-08','Instruktor 1. kat - Prva liga Srbije','sudija.jpg'),(2,'Zoran','Petrović',NULL,'Super/Prva liga Srbije','sudija.jpg'),(3,'Dejan','Najdić','1974-10-04','Instruktor 1. kat - Prva liga Srbije','sudija.jpg'),(4,'Ivica','Milojković','1976-05-07','Srpska liga Istok','sudija.jpg'),(5,'Stefan','Stevanović','1990-04-11','Okružna liga Zaječar','sudija.jpg'),(6,'Zvonko','Trifunović',NULL,'Opštinska liga Sokobanja','sudija.jpg'),(7,'Vlada','Manić','1953-04-28','Opštinska liga Sokobanja','sudija.jpg'),(8,'Milan','Gavrilović','1983-02-10','Srpska liga Istok','sudija.jpg'),(9,'Slaviša','Markovic',NULL,'Zona Istok','sudija.jpg'),(10,'Darko','Ognjanović',NULL,'Okružna liga Zaječar','sudija.jpg'),(11,'Aleksandar','Marjanović','1985-08-02','Okružna liga Zaječar','sudija.jpg'),(12,'Darko','Pavlović','1974-06-24','Opštinska liga Sokobanja','sudija.jpg'),(13,'Radomir  Leša','Milovanović',NULL,'Opštinska liga Sokobanja','sudija.jpg'),(14,'Stefan','Miletić','1997-06-19','Srpska liga Istok','sudija.jpg'),(15,'Vlada','Manić',NULL,'Opštinska liga Sokobanja','sudija.jpg'),(16,'Bogdan','Tomić','1996-05-28','Srpska liga Istok','sudija.jpg');
+/*!40000 ALTER TABLE `fudbal_delegat` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-13 15:01:29
---
+++ Sema-podaci iz baze/nova_baza/fudbal_sb_fudbal_vest0.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
+--
+-- Host: localhost    Database: fudbal_sb
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `fudbal_vest`
+--
+
+LOCK TABLES `fudbal_vest` WRITE;
+/*!40000 ALTER TABLE `fudbal_vest` DISABLE KEYS */;
+INSERT INTO `fudbal_vest` VALUES (1,'Izabarano novo rukovodstvo Fudbalskog saveza opštine Sokobanja','Izborna skupština Fudbalskog saveza opštine Sokobanja održana je 24. februara. Na skupštini je bilo prisutno 14 delegata koji su jednoglasno za predsednika Fudbalskog saveza opštine Sokobanja izabrali Milana Gavrilovića koji je na toj funkciji zamenio, dosadašnjeg predsednika, Đorđa Bajića.\r\nNa skupštini su, nakon izbora predsednika, izabrani i članovi izvršnog i nadzornog odbora. Izvršni odbor činiće Milan Urošević, Milija Milenković, Dejan Najdić, Slobodan Paunović, Saša Mihailović, Vojkan Milanović i Milan Gavrilović, dok su za članove nadzornog odbora izabrani Zoran Šujica, Slaviša Marković i Dejan Stanković. Nakon izborne skupštine održan je izvršni odbor na kome je izabran potpredsednik saveza. Na tu funkciju postavljen je Milan Urošević. Funkciju generalnog sekretara vršiće Miljan Marković iz Vrbovca.\r\n Fudbalski savez Srbije je raspisao izbore na svojoj sednici izvršnog odbora za teritoriju cele zemlje i odredio rokove u okviru kojih to mora da se završi. Za opštinske saveze rok je bio 24. februar tekuće godine da bi sve bilo završeno do 17. marta kada će biti izabran predsednik Fudbalskog saveza Srbije ‒ kaže Milan Gavrilović predsednik Fudbalskog saveza opštine Sokobanja. Izvor: Sokopress','2019-02-26 13:34:50.000000');
+/*!40000 ALTER TABLE `fudbal_vest` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-13 15:01:24
